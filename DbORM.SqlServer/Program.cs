8a864a0f19b14995b19e61e487730f5bc7ac9783
richie <richie.kwon1@gmail.com>
2022-10-31 20:55:18 +0000

    Initial commit

---
+++ DbORM.SqlServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DbORM.SqlServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
